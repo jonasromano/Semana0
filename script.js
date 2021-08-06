fb6836b29250f6b54ac61410c6531dece3694f1f
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-08-06 11:03:06 -0300

    ajuste
    Ajuste de la imagen de fondo

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function saludar() {
 }
 
 saludar();
+
+let json_url = "https://jonasromano.github.io/Semana0/listado.json"
+
+function datos(json_url){
+    document.getElementById().innerHTML="";
+    fetch(url).then(respuesta => respuesta.json())
+}
